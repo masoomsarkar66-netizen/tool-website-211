6a6907726cce13405b4a5e0e839d87856a14881e
masoomsarkar66-netizen <masoomsarkar66@gmail.com>
2026-07-15 04:44:02 +0500

    Create referenceRanges.ts

---
+++ src/utils/referenceRanges.ts
@@ -0,0 +1,25 @@
+export const referenceRanges = {
+  hemoglobin: {
+    min: 13.5,
+    max: 17.5,
+    unit: "g/dL",
+  },
+
+  wbc: {
+    min: 4,
+    max: 11,
+    unit: "×10⁹/L",
+  },
+
+  rbc: {
+    min: 4.5,
+    max: 5.9,
+    unit: "×10¹²/L",
+  },
+
+  platelets: {
+    min: 150,
+    max: 450,
+    unit: "×10⁹/L",
+  }
+};
