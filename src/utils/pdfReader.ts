ffe281c8a6d916a7fd328f0211efaec537b38d26
masoomsarkar66-netizen <masoomsarkar66@gmail.com>
2026-07-15 23:30:03 +0500

    Update pdfReader.ts

--- src/utils/pdfReader.ts
+++ src/utils/pdfReader.ts
@@ -1,19 +1,47 @@
-export async function extractPDFText(file: File) {
-  const fileSize = (file.size / 1024).toFixed(2);
+import * as pdfjsLib from "pdfjs-dist";
+import workerSrc from "pdfjs-dist/build/pdf.worker.min.mjs?url";
 
-  return `
-PDF Upload Successful
 
-File Name:
-${file.name}
+pdfjsLib.GlobalWorkerOptions.workerSrc = workerSrc;
 
-File Size:
-${fileSize} KB
 
-Status:
-PDF received successfully.
+export async function extractPDFText(
+  file: File
+): Promise<string> {
 
-Note:
-Text extraction will be connected in the next step.
-`;
+  const arrayBuffer = await file.arrayBuffer();
+
+
+  const pdf = await pdfjsLib.getDocument({
+    data: arrayBuffer,
+  }).promise;
+
+
+  let fullText = "";
+
+
+  for (
+    let pageNumber = 1;
+    pageNumber <= pdf.numPages;
+    pageNumber++
+  ) {
+
+    const page = await pdf.getPage(pageNumber);
+
+
+    const textContent =
+      await page.getTextContent();
+
+
+    const pageText =
+      textContent.items
+        .map((item: any) => item.str)
+        .join(" ");
+
+
+    fullText += pageText + "\n";
+  }
+
+
+  return fullText;
 }
