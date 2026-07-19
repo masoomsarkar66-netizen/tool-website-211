f8017d32056a19c8492b9ce5027375087ff0be44
masoomsarkar66-netizen <masoomsarkar66@gmail.com>
2026-07-19 23:11:37 +0500

    Update pdfReader.ts

--- src/utils/pdfReader.ts
+++ src/utils/pdfReader.ts
@@ -1,47 +1,45 @@
 import * as pdfjsLib from "pdfjs-dist";
 import workerSrc from "pdfjs-dist/build/pdf.worker.min.mjs?url";
 
-
+// PDF Worker
 pdfjsLib.GlobalWorkerOptions.workerSrc = workerSrc;
 
+export async function extractPDFText(file: File): Promise<string> {
+  try {
+    const arrayBuffer = await file.arrayBuffer();
 
-export async function extractPDFText(
-  file: File
-): Promise<string> {
+    const loadingTask = pdfjsLib.getDocument({
+      data: arrayBuffer,
+    });
 
-  const arrayBuffer = await file.arrayBuffer();
+    const pdf = await loadingTask.promise;
 
+    let fullText = "";
 
-  const pdf = await pdfjsLib.getDocument({
-    data: arrayBuffer,
-  }).promise;
+    for (let pageNumber = 1; pageNumber <= pdf.numPages; pageNumber++) {
+      const page = await pdf.getPage(pageNumber);
 
+      const textContent = await page.getTextContent();
 
-  let fullText = "";
-
-
-  for (
-    let pageNumber = 1;
-    pageNumber <= pdf.numPages;
-    pageNumber++
-  ) {
-
-    const page = await pdf.getPage(pageNumber);
-
-
-    const textContent =
-      await page.getTextContent();
-
-
-    const pageText =
-      textContent.items
-        .map((item: any) => item.str)
+      const pageText = textContent.items
+        .map((item: any) => {
+          if ("str" in item) {
+            return item.str;
+          }
+          return "";
+        })
         .join(" ");
 
+      fullText += pageText + "\n";
+    }
 
-    fullText += pageText + "\n";
+    console.log("========== PDF TEXT ==========");
+    console.log(fullText);
+    console.log("==============================");
+
+    return fullText.trim();
+  } catch (error) {
+    console.error("PDF Reader Error:", error);
+    throw new Error("Unable to read PDF.");
   }
-
-
-  return fullText;
 }
